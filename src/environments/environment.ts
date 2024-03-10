69bb4659868ddc5e27e429750aee28443fc6b0d9
Angel Hermozo <mickyhermozo72@gmail.com>
2024-03-10 07:51:36 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyANzpIezB8dGmhJ9PxeBXLtL-Cderfd45',
-    authDomain: 'pruebaapp-84402.firebaseapp.com',
-    databaseURL: 'https://pruebaapp-84402-default-rtdb.firebaseio.com',
-    projectId: 'pruebaapp-84402',
-    storageBucket: 'pruebaapp-84402.appspot.com',
-    messagingSenderId: '714149177679',
-    appId: '1:714149177679:web:725e6b124fb3fc9964bd3a'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 };
